49341ac4837321bf6d03f4b28abf21ac9506bcfe
JeckaSh <jeckashevelkov@icloud.com>
2026-05-29 21:35:32 +0300

    page object fix 3

--- pages/base_page.py
+++ pages/base_page.py
@@ -1,5 +1,4 @@
 import allure
-from selenium.webdriver.common.by import By
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.common.exceptions import TimeoutException
 from selenium.webdriver.support import expected_conditions as ec
